86d52fe6a98e29da5edeaab230c814c521061260
Athira TU <aathuathira700@gmail.com>
2023-03-15 08:27:08 +0530

    edited signup page using useRef

--- src/components/signup.jsx
+++ src/components/signup.jsx
@@ -1,60 +1,53 @@
-import React, { useState } from 'react'
+import React, { useRef} from 'react'
 import { useNavigate } from 'react-router-dom'
 
-function SignUp(props) {
+function SignUp() {
+
+  
+  const emailRef=useRef()
+  const usernameRef=useRef()
+  const phoneRef=useRef()
+  const passwordRef=useRef()
 
 
-    const[email,setemail]=useState("")
-const[username,setusername]=useState("")
-const[phone,setphone]=useState("")
-const[password,setpass]=useState("")
+//     const[email,setemail]=useState("")
+// const[username,setusername]=useState("")
+// const[phone,setphone]=useState("")
+// const[password,setpass]=useState("")
 
 const navigate=useNavigate()
 
-const[signup,setsignup]=useState(false)
+// const[signup,setsignup]=useState(false)
+// signupRef=useRef()
 
 function showData(){
-   setsignup(true)
+
    let obj={
-    email,
-    password,
-    username,
-    phone
+    email: emailRef.current.value,
+    password: passwordRef.current.value,
+    username: usernameRef.current.value,
+    phone: phoneRef.current.value
    }
-   props.setuser(obj)
    navigate('/login')
-   console.log(email)
-   console.log(obj);
+   
 }
-function reset(){
-    setemail("")    
-    setusername("")
-    setpass("")
-    setphone("")
-    setsignup(false)
-}
+
 
 
   return (
     <div>
         <label htmlFor="">Email: </label>
-        <input value={email} type="email" onChange={(e)=>{setemail(e.target.value);}} /><br /><br />
+        <input  type="email" ref={emailRef} /><br /><br />
         <label  htmlFor="">UserName: </label>
-        <input value={username} type="text" onChange={(n)=>{setusername(n.target.value);}} /><br /><br />
+        <input type="text" ref={usernameRef} /><br /><br />
         <label htmlFor="">Phone: </label>
-        <input value={phone} type="text" onChange={(p)=>{setphone(p.target.value);}} /><br /><br />
+        <input  type="text"ref={phoneRef} /><br /><br />
         <label htmlFor="">Password: </label>
-        <input value={password} type="password" onChange={(ps)=>{setpass(ps.target.value);}} /><br /><br />
+        <input  type="password" ref={passwordRef}/><br /><br />
         <button onClick={showData}>SignUp</button>
-        <button onClick={reset}>Reset</button>
-{signup&&
-<>
-        <p>{email}</p>
-        <p>{username}</p>
-        <p>{phone}</p>
-        <p>{password}</p>
-</>
-}
+
+
+    
     </div>
   )
 }
